c68f909e8da3d453b5ea02b4731a56a007db0e3e
Juan Pablo Barreto <juampi92@gmail.com>
2025-03-10 10:05:51 +0100

    first android build

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
   orientation: 'portrait',
   icon: './assets/icon.png',
   splash: {
-    image: './assets/splash.png',
+    image: './assets/splash-icon.png',
     resizeMode: 'contain',
     backgroundColor: '#1a237e',
   },
@@ -22,8 +22,14 @@ export default {
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#1a237e',
     },
+    package: 'com.juampi92.lunarlog',
   },
   web: {
     favicon: './assets/favicon.png',
+  },
+  extra: {
+    eas: {
+      projectId: 'eb94ad84-8892-49e9-ae18-932da5f8a672',
+    },
   },
 };
